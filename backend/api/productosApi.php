40e5e1cf08cce487872a9458ab01d9d4dbaa91a6
Eduardo Salavarria <eesalava@espol.edu.ec>
2023-01-21 23:56:53 -0500

    correccion de problema con CORS

--- backend/api/productosApi.php
+++ backend/api/productosApi.php
@@ -1,4 +1,16 @@
 <?php
+ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
 //echo "metodo http: ".$_SERVER['REQUEST_METHOD'];
 //echo 'Informacion:' . file_get_contents('php://input'); //lee lo que mando el cliente
--- backend/api/vendedoresApi.php
+++ backend/api/vendedoresApi.php
@@ -4,6 +4,19 @@
 //echo 'Informacion:' . file_get_contents('php://input'); //lee lo que mando el cliente
 //indicar que estamos mandando un json
 header('Content-Type: application/json; charset=utf-8'); //indico que le estoy mandando un json
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
 include_once("../clases/vendedor.php");
 
 switch($_SERVER['REQUEST_METHOD']){
